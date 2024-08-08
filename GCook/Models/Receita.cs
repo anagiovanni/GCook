c7cbbf38921a158fe120a7b103cfed2de690145f
anagiovanni <anagiovanni66@gmail.com>
2024-08-08 12:22:56 -0300

    Criado o projeto e iniciado a camada de dados

---
+++ GCook/Models/Receita.cs
@@ -0,0 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GCook.Models;
+[Table("Receita")]
+public class Receita
+{
+    [Key]
+    public int Id { get; set; }
+}
